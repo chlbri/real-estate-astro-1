f2d0484338d8b63ff0cce0279170bebd2974a94f
chlbri <bri_lvi@icloud.com>
2025-01-05 20:14:29 +0000

    builkd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
+  adapter: vercel({ imageService: true }),
 });
